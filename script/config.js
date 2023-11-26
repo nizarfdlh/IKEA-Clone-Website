96c7c5f70c8c3533c1d2f8ae237755068aba2d04
nizarfdlh <suncodev@gmail.com>
2023-11-27 06:15:15 +0700

    initial commit

---
+++ script/config.js
@@ -0,0 +1,12 @@
+function toggleForm() {
+    var loginForm = document.getElementById('loginForm');
+    var registerForm = document.getElementById('registerForm');
+
+    if (loginForm.style.display === 'none') {
+        loginForm.style.display = 'block';
+        registerForm.style.display = 'none';
+    } else {
+        loginForm.style.display = 'none';
+        registerForm.style.display = 'block';
+    }
+}
